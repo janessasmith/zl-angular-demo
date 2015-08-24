5a8b0292ba35db1c3c08fdf1a03e07b542c58751
janessasmith <jupiter_zheng@sina.com>
2015-08-24 13:36:47 +0800

    fix bug

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,36 +12,55 @@ angular.module('formApp', [
 .config(function($stateProvider, $urlRouterProvider) {
 
     $stateProvider
-    // route to show our basic form (/form)
-    .state('form', {
-        url: '/form',
-        templateUrl: '../views/forms/form.html',
-        controller: 'formController'
-    })
+        .state('form', {
+            url: '/form',
+            templateUrl: 'views/forms/form.html',
+            controller: 'formController',
+        })
 
-    // nested states
-    // each of these sections will have their own view
-    // url will be nested (/form/profile)
-    .state('form.profile', {
-        url: '/profile',
-        templateUrl: '../views/forms/form-profile.html'
-    })
+        // nested states
+        // each of these sections will have their own view
+        // url will be nested (/form/profile)
+        .state('form.profile', {
+            url: '/profile',
+            templateUrl: 'views/forms/form-profile.html'
+        })
 
-    // url will be /form/interests
-    .state('form.interests', {
-        url: '/interests',
-        templateUrl: '../views/forms/form-interests.html'
-    })
+        // url will be /form/interests
+        .state('form.interests', {
+            url: '/interests',
+            templateUrl: 'views/forms/form-interests.html'
+        })
 
-    // url will be /form/payment
-    .state('form.payment', {
-        url: '/payment',
-        templateUrl: '../views/forms/form-payment.html'
-    });
+        // url will be /form/payment
+        .state('form.payment', {
+            url: '/payment',
+            templateUrl: 'views/forms/form-payment.html'
+        });
 
-    // catch all route
-    // send users to the form page
-    $urlRouterProvider.otherwise('/form/profile');
+        // catch all route
+        // send users to the form page
+        $urlRouterProvider.otherwise('/form/profile');
+
+
+        // route to show our basic form (/form)
+        /*.state('index', {
+            url: '/index',
+            views: {
+                '': {
+                    templateUrl: 'views/forms/form.html'
+                },
+                'formProfile@index': {
+                    templateUrl: 'views/forms/form-profile.html'
+                },
+                'formInterests@index': {
+                    templateUrl: 'views/forms/form-interests.html'
+                },
+                'formPayment@index': {
+                    templateUrl: 'views/forms/form-payment.html'
+                }
+            }
+         })*/
 })
 
 // our controller for the form
